ef9c95979c28b1bebc4079bcf02493720de3670b
Kirsten McMillan <kirsten.marie.mcmillan@gmail.com>
2023-11-09 13:24:23 +0000

    Add files via upload

--- Source code 2. Simulations and figure generation.R
+++ Source code 2. Simulations and figure generation.R
@@ -1,17 +1,9 @@
-#rm( list=ls() )
-setwd("~/Documents/Documents - Kirsten’s MacBook Pro/Dogs Trust/UKPDPP")
-
 install.packages(R2jags)
 library(R2jags)
 
 # IMPORT DATA ##################################################################
 ################################################################################
-FINAL_ALL_DEDUP<-read.csv("FINAL_DE-DUPED_UKPDPP_29.01.21.csv", check.names=FALSE, na.strings=c("","NA"), encoding = "UTF-8")
-nrow(FINAL_ALL_DEDUP) #4,375,861
-table(FINAL_ALL_DEDUP$PROVIDER,useNA = "always")
-colnames(FINAL_ALL_DEDUP)
-sapply(FINAL_ALL_DEDUP,class)
-
+FINAL_ALL_DEDUP<-read.csv("FINAL_DE-DUPED_UKPDPP.csv", check.names=FALSE, na.strings=c("","NA"), encoding = "UTF-8")
 cols1 <- c("BREED_CLEAN","PROVIDER","BREED_CLEAN.datasvis","CROSSBREED","POSTCODE_2","SEX","DATE_OF_BIRTH.clean.year.month","STATUS","NEUTERED")
 cols2 <- c("DATE_OF_BIRTH.clean","TERMINATION_DATE.clean")
 
@@ -23,35 +15,30 @@ FINAL_ALL_DEDUP <- FINAL_ALL_DEDUP %>%
 #Estimated TOTAL no. of dogs per postcode (from Aegerter et al. 2017)
 #https://data.gov.uk/dataset/ec8fc820-2e36-49d0-a09c-e2901e10b2e4/dog-population-per-postcode-district
 dog_estimate <- read.csv("Estimated_dog_pop_per_postcode.csv", check.names=FALSE, na.strings=c("","NA"))
-N_sum<-sum(as.integer(dog_estimate$TOTAL_dogs)) # 11,884,824
+N_sum<-sum(as.integer(dog_estimate$TOTAL_dogs))
 
 #TOTAL human population per postcode
 human_population <- read.csv("UK human population by postcode.csv", check.names=FALSE, na.strings=c("","NA"))
 human_pop_scaled<-as.numeric(scale(human_population[,2]))
-N_sum_human<-sum(as.integer(human_population$human_population_2011)) # 63,305,852
+N_sum_human<-sum(as.integer(human_population$human_population_2011))
 ################################################################################
 
-# SUBSET TO ALIVE ONLY AND UNDER 18.3 years (95% dead in survival analysis) ####
+# SUBSET TO ALIVE ONLY AND UNDER 18.3 years (95% dead in survival analysis - McMillan et al., submitted) ####
 ################################################################################
 FINAL_ALL_DEDUP_sub<-subset(FINAL_ALL_DEDUP, AGE_YRS_CALC <= 18.3 & STATUS == "Alive")
-table(FINAL_ALL_DEDUP_sub$STATUS)
-table(FINAL_ALL_DEDUP_sub$AGE_YRS_CALC)
-nrow(FINAL_ALL_DEDUP_sub)
-table(FINAL_ALL_DEDUP_sub$STATUS)
 ################################################################################
 
 # CREATE MATRIX ################################################################
 ################################################################################
 unique(FINAL_ALL_DEDUP$POSTCODE_2) #124 postcodes
 
-dog_per_postcode_per_survey<-table(FINAL_ALL_DEDUP$POSTCODE_2, FINAL_ALL_DEDUP$PROVIDER) # 11 providers
+dog_per_postcode_per_survey<-table(FINAL_ALL_DEDUP$POSTCODE_2, FINAL_ALL_DEDUP$PROVIDER)
 n_ij_dedup<-as.matrix.data.frame(dog_per_postcode_per_survey)
   write.csv(n_ij_dedup,'n_ij_dedup.csv')
 ################################################################################
 
 
 ########################## DATA ####
-
 #All Dogs Matrix
     all_ij<-read.csv('n_ij_dedup.csv',header=T)
     all_ij<-all_ij[,-1]
@@ -59,12 +46,7 @@ n_ij_dedup<-as.matrix.data.frame(dog_per_postcode_per_survey)
   #Total Count of Dogs for Poisson-Gamma Mixture Models  
     Cval=sum(all_ij)
     
-#Insurance Matrix
-#    insurance_ij<-read.csv('n_ij_DOUBLE_DEDUP_INSURANCE.csv',header=T)
-#      insurance_ij<-insurance_ij[,-1]
-
     
-
 ######## RNG Seed ####
     set.seed(123)
     
@@ -167,7 +149,7 @@ n_ij_dedup<-as.matrix.data.frame(dog_per_postcode_per_survey)
      
      
      nmix_random_out<-  jags_nmix_random$BUGSoutput$summary
-     write.csv(nmix_random_out, file = "/Users/Kirsten/Documents/Documents - Kirsten’s MacBook Pro/Dogs Trust/UKPDPP/nmix_random_out_summary.csv")
+     write.csv(nmix_random_out, file = "")
      
       head(nmix_random_out)
      
@@ -280,135 +262,3 @@ n_ij_dedup<-as.matrix.data.frame(dog_per_postcode_per_survey)
           dogs_capita_plot4
             ggsave2('Dogs Per Capita Versus Human Pop Size.pdf',dogs_capita_plot4)
           
-            
-      ##Outlier??
-          subset(dogs_per_capita_stats,posterior_mean>1) #census data v low 
-            
-            
-            
-            
-            
-      # #######################
-      # #        
-      # # MODEL 1: N-MIXTURE MODEL WITH FIXED EFFECTS FOR INTERCEPTS AND SLOPES (NOT DRAWN FROM A COMMON DISTRIBUTION)
-      # #        
-      # #######################                   
-      # 
-      # ########### Model - Overdispersed Poisson with Observation Level Random Effect
-      # 
-      # model_code_olre = '
-      #               model {
-      #                 for(i in 1:I) { # Loop through postcode
-      #                   for(j in 1:J) { # Loop through survey
-      #                     n_ij[i,j] ~ dbinom(p_ij[i,j], N_i[i])
-      #                     logit(p_ij[i,j]) = alpha[j] + beta[j] * human_pop[i]
-      #                   }
-      #                   N_i[i] ~ dpois(lambda[i]) # lambda is the prior estimate of number of dogs in this postcode
-      #                   log(lambda[i]) <-  alpha.mu[i]
-      #                     }
-      #                 
-      #                 for(i in 1:I){
-      #                     alpha.mu[i]~dnorm(0,tau.olre)
-      #                   }
-      #                   
-      #                   sd.olre ~ dunif(0,2)
-      #                   tau.olre <- 1/(sd.olre*sd.olre)
-      #                   
-      #                 
-      #                   beta.mu ~ dnorm(0,0.001)
-      #                 
-      #                 for (j in 1:J) {
-      #                   alpha[j] ~ dnorm(0, 0.001)
-      #                   beta[j] ~ dnorm(0, 0.001)
-      #                 }
-      #                 
-      #                 
-      #                 N = sum(N_i) # Sum up all the populations in each postcode to give total estimate
-      #               }
-      #               
-      #                      ' ##end model 
-      # 
-      # 
-      # ##Inits  
-      # Nst <- apply(all_ij, 1, sum) + 1 #maximum count per row of n_ij
-      # inits_od <- function(){list(N_i = Nst, alpha.mu=rnorm(124, 0, 1), 
-      #                             alpha=rnorm(ncol(all_ij), 0, 1), beta=rnorm(ncol(all_ij), 0, 1))}
-      # 
-      # ####### Model 
-      # jags_all_od = jags(data = list(I = nrow(all_ij), # The number of post-codes
-      #                                J = ncol(all_ij), # The number of surveys
-      #                                human_pop=human_pop_scaled,
-      #                                n_ij = all_ij), # Counts of dogs in each postcode (row) and each survey (column)
-      #                    inits=inits_od, #starting values
-      #                    parameters.to.save = c('N', 'alpha','beta', 'alpha.mu','beta.mu','lambda',"N_i","tau.olre"),
-      #                    model.file = textConnection(model_code_olre),n.iter=100000,n.burnin=10000,n.thin=50)
-      # 
-      # #Check 
-      # n_plot(jags_all_od)
-      # n_plot(jags_all_od2)
-      # 
-      # #Store Output   
-      # jags_all_od_out<- jags_all_od$BUGSoutput$summary
-      # head(jags_all_od_out)
-      # tail(jags_all_od_out,40)    
-      # 
-      # 
-      # #Strip Out MCMC Chains 
-      # jags_all_od_out_mcmc<-as.data.frame(jags_all_od$BUGSoutput$sims.matrix)
-      # head(jags_all_od_out_mcmc)
-      # 
-      # #Rename N_i with Postcodes
-      # colnames(jags_all_od_out_mcmc)[grepl("N_i",colnames(jags_all_od_out_mcmc))]<-rownames(dog_per_postcode_per_survey)
-      # colnames(jags_all_od_out_mcmc)[1]<-"N_total"
-      # 
-      # #Long Format 
-      # jags_all_od_out_mcmc %>% pivot_longer(colnames(jags_all_od_out_mcmc),names_to="parameter",values_to="value") -> jags_all_od_plotdata
-      # 
-      # #Plot
-      # jags_all_od_plotdata %>% filter(parameter=="N_total") %>% ggplot(aes(y=parameter,x=value)) + stat_halfeye(shape=21,point_fill="white",point_size=5,colour="black") + theme_bw()
-      # 
-      # 
-      # ### Plot of Postcode Specific Ns
-      # jags_postcode_plotdata <- jags_all_od_plotdata %>% filter(parameter %in% rownames(dog_per_postcode_per_survey))
-      # pc1<-ggplot(jags_postcode_plotdata,aes(y=parameter,x=value)) + stat_halfeye(shape=21,point_fill="white",point_size=5,colour="black") + theme_bw()
-      # pc1
-      # 
-      # 
-      # ########### Dataframe Assembly 
-      # # Model Posterior Means 
-      # # Raw Input Data 
-      # # Previous Per-Postcode Estimates
-      # 
-      # #Summarise Model Means  
-      # pc_aggregate_data<-jags_postcode_plotdata %>% group_by(parameter) %>% summarize(posterior_mean= mean(value))
-      # 
-      # #Add Input Data  
-      # pc_aggregate_data$raw_data<- apply(all_ij,1,sum)  
-      # head(pc_aggregate_data)
-      # 
-      # #Add Previous Estimates    
-      # pc_aggregate_data$previous_estimate_Aegerter<- dog_estimate[,2]  
-      # 
-      # #Add Human Pop Size      
-      # pc_aggregate_data $human_pop2011<- human_population[,2]
-      # head(pc_aggregate_data)
-      # 
-      # 
-      # ########### Plots
-      # 
-      # ### Our model versus Aegerter - look for Outliers
-      # nmix_aegerter1<- ggplot(pc_aggregate_data,aes(x=previous_estimate_Aegerter,y=posterior_mean)) + geom_point(size=5,shape=21,fill="white") + theme_bw() + geom_abline(intercept=0,slope=1)
-      # 
-      # ### Our model versus Human Pop 
-      # nmix_human1<- ggplot(pc_aggregate_data,aes(x=human_pop2011,y=posterior_mean)) + geom_point(size=5,shape=21,fill="white") + theme_bw() + geom_smooth(method="lm")
-      # 
-      
-      
-      
-      
-      
-      
-      
-     
-     
-    
